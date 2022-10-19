e8863d7f864f887186d27d3494009c562ee060e1
evanRez <ereznice@gmail.com>
2022-10-18 21:26:09 -0400

    Using cmd line saves

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using WebAPIClient;
+
+internal class Program
+{
+    private static readonly HttpClient client = new HttpClient();
+    static async Task Main(string[] args)
+    {
+        await ProcessRepositories();
+    }
+
+    private static async Task ProcessRepositories()
+    {
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")
+        );
+        client.DefaultRequestHeaders.Add("User-Agent", ".Net Foundation Repository Reporter");
+
+        var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+        var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+
+        foreach (var repo in repositories)
+            Console.WriteLine(repo.Name);
+    }
+}
---
+++ repo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace WebAPIClient
+{
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        
+    }
+}
